e04612dddd0211fbfd7b0b0cbc395deed8384fe2
Robin Jones <RobinJones335@gmail.com>
2020-04-02 18:52:21 -0400

    removed node modules

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (name, eaten) VALUES ('Vegan Burger', FALSE);
-INSERT INTO burgers (name, eaten) VALUES ('Shurmburgers', FALSE);
+INSERT INTO burgers (name, eaten) VALUES ('Shurmburgers', TRUE);
 INSERT INTO burgers (name, eaten) VALUES ('PhillyCheesteak burger', FALSE);
